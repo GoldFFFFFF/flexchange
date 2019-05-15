be31cf705d0ceb6ab6720f2e5a549be61802817d
GoldFFFFFF <1284688865@qq.com>
2019-05-16 03:52:51 +0800

    for log in

--- src/pages/homepage/index.vue
+++ src/pages/homepage/index.vue
@@ -1,58 +1,42 @@
 <template>
-  <div >
-    <div class='page_row' >
+  <div class="wholePage"> 
 
-      <div class="search">
-        <div class="search-bar">
-            <icon class="searchIcon" size='20' type='search'></icon>
-            <input class="" placeholder="ITEM NAME..." v-model="searchInput">
-        </div>
-      </div>
-
-      <div class='button' @click="bindSearch">
-        <button type="default" size="mini" bindtap="default" hover-class="button-hover"> Search</button>
-      </div>
-
-      <div class='filter' style="" @click="bindNewItem" >
-        <img src="/static/icons/more.png" style="height:72rpx;width:72rpx;"/>
-      </div>
-      
+    <div v-if="!logged">
+      <div id="text">Please log in!</div>
+      <button id="login" @click='logIn'> LogIn</button>
     </div>
+    
+    <div v-else class='page_row' >
+      <div class="search">
+        <div class="search-bar" >
+            <!-- <icon class="searchIcon" size='20' type='search'></icon> -->
+            <input class="searchText" placeholder="ITEM NAME..." v-model="searchInput">
+        </div>
+        <div class='button' @click="bindSearch">
+          <button type="default" size="mini" bindtap="default" hover-class="button-hover"> Search</button>
+        </div>
 
-    <div class='show_item' v-for='item in items' :key=item.name>
-          <mainItem :imgUrl='item.imgUrl' 
+        <div class='filter' style="" @click="bindNewItem" >
+          <img src="/static/icons/more.png" style="height:72rpx;width:72rpx;"/>
+        </div>
+      </div>     
+
+      <div class='show_item' >
+          <div class='item' v-for='item in items' :key=item.name>
+              <mainItem :imgUrl='item.imgUrl' 
                     :name='item.name' 
                     :price='item.price' 
                     :type='item.type'
                     :description='item.description'
                     :status='item.status'
                     :itemId='item._id'>
-          </mainItem>
+              </mainItem>
+          </div>
+      </div>
+
     </div>
-    <!--div class='swiper-bg'>
-    <div class="image-swiper">
-          <swiper :indicator-dots="indicatorDots" 
-            :autoplay="autoplay" 
-            :interval="interval" 
-            :duration="duration" 
-            :circular="circular"
-            :previous-margin="'75px'"
-            @change="swiperChange" 
-            @animationfinish="animationfinish">
-            <div v-for='item in imgUrls' :key='item.id'>
-              <swiper-item>
-                <image :src="item" class="slide-image" />
-              </swiper-item>
-            </div>
-          </swiper>
-        </div>
-    </div!-->
-      <!-- <comp img_path="/static/images/Yukino.jpg" item_name='Yukinooos photo' item_price='$100'></comp>
-      <div class='item-2' style='position:absolute;top:350px' @click='bindDetails'>
-        <comp img_path='/static/images/wolaopo.jpg' item_name='wolaopossss' item_price='$9999'></comp>
-      </div> -->
-      
-</div>
+
+  </div>
 
 </template>
 
@@ -63,13 +47,32 @@ export default {
   data () {
     return {
       searchInput: '',
-      items: []
+      items: [],
+      logged: false
     }
   },
   components: {
     mainItem
   },
   methods: {
+    logIn () {
+      this.logged = true
+      // wx.login({
+      //   success: function (res) {
+      //     if (res.code) {
+      //     // 发起网络请求
+      //       wx.request({
+      //         url: 'https://test.com/onLogin',
+      //         data: {
+      //           code: res.code
+      //         }
+      //       })
+      //     } else {
+      //       console.log('登录失败！' + res.errMsg)
+      //     }
+      //   }
+      // })
+    },
     bindSearch () {
       wx.navigateTo({
         url: '../item/main?description=' + this.searchInput + '&price' + this.items.price
@@ -83,93 +86,91 @@ export default {
     bindNewItem () {
       const url = '../shoppingCar/main'
       wx.switchTab({ url })
+    },
+    getPage () {
+      this.$ajax.get({
+        url: 'http://203.195.164.28:3000/api/item/'
+      }).then((res) => {
+        this.items = res.data.resultMessage
+        setTimeout(() => {
+          this.freshPos = 0
+        }, 300)
+        wx.hideToast()
+      }).catch((err) => {
+        console.log(err)
+        setTimeout(() => {
+          this.freshPos = 0
+        }, 300)
+        wx.hideToast()
+      })
     }
   },
   created () {
-    this.$ajax.get({
-      url: 'http://203.195.164.28:3000/api/item/'
-    }).then((res) => {
-      this.items = res.data.resultMessage
-      console.log(this.items)
-      setTimeout(() => {
-        this.freshPos = 0
-      }, 300)
-      wx.hideToast()
-    }).catch((err) => {
-      console.log(err)
-      setTimeout(() => {
-        this.freshPos = 0
-      }, 300)
-      wx.hideToast()
-    })
+    this.getPage()
+  },
+  onPullDownRefresh: function () {
+    this.getPage()
   }
 }
 </script>
 
 <style>
-.post-divide-line{
-  position:relative;
-  margin-top:0;
-  height: 1px;
-  background-color:aqua;
+#text{
+  height: 350px;
+  line-height: 350px;
+  text-align: center;
+}
+
+#login{
+  margin: 19%;
+}
+
+.wholePage{
   width: 100%;
+  height: 100%;
 }
-.swiper-bg{
-  background-color:grey;
-}
-.image-swiper{
-  padding:10px 10px 10px 10px;
-}
-.slide-image {
-  display:flex;
-  width:200px;
-  height:200px;
-}
-.container log-list{
-  top:150px;
-  left:150px;
-}
+
 .item{
   width:150px;
   height:150px;
-  /*border-radius: 50%;*/
 }
-.hover-icon{
-  opacity:0.9;
-  background:grey;
-}
+
 .filter{
+  float: left;
   position:relative;
   margin-top:-5rpx;
   margin-left:10rpx;
   margin-right:10rpx;
 }
 .search{
-  width:120%;
+  display: flex;
 }
 .page_row{
-  margin-top:20rpx;
-  flex-direction: row;
-  display:flex;
+  display: flex;
+  flex-direction: column;
 }
 .search-bar{
-  display:flex;
-  border: 1px solid #d0d0d0;
+  width: 55%;
+  border: 1.5px solid #1f1f1f;
   border-radius: 10rpx;
-  margin-left: 20rpx;
+  margin-left: 40rpx;
+  margin-bottom: 20rpx;
 }
 .search-bar input{
+  float: left;
   padding-top:10rpx;
   padding-left: 20rpx;
 }
 
 .button{
+  float: left;
   position:relative;
-  margin-left:30rpx;
+  margin-left:20rpx;
+  margin-right:10rpx;
   text-align:center;
 }
 .button-hover{
-  background-color:red;
+  background-color:#1f1f1f;
 }
 .searchIcon{
   margin:10rpx 10rpx 10rpx 10rpx;
@@ -177,6 +178,17 @@ export default {
   z-index:2;
   width:20px;
   height: 20px;
-  text-align:center;
 }
+.show_item{
+  padding-left: 20px;
+  padding-top: 10px;
+  width: 100%;
+  display: relative;
+}
+.item{
+  width: 50%;
+  height: 220px;
+  float: left;
+}
+
 </style>
