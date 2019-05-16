312475b8b09f6726bb595926cd06d46859bd0a5a
Yukinooo7 <784415630@qq.com>
2019-05-17 01:53:51 +0800

    add filter page

---
+++ src/pages/filter/main.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import App from './index'
+
+const app = new Vue(App)
+app.$mount()
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  state: {
+    count: 0
+  },
+  mutations: {
+    increment: (state) => {
+      const obj = state
+      obj.count += 1
+    },
+    decrement: (state) => {
+      const obj = state
+      obj.count -= 1
+    }
+  }
+})
+
+export default store
